298a2664c2c45cf297f5eb7bb136bc1c10b25bd6
MarcellaLeite <marcellasaleite@gmail.com>
2023-01-17 17:51:13 -0300

    main

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,9 @@
 const cipher = {
+  var button1 = document.querySelector("#submit1");
+    button1.addEventListener("click",verifica1)
+
+    var button2 = document.querySelector("#submit2");
+    button2.addEventListener("click",verifica2)
   // ...
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+var button1 = document.querySelector("#submit1");
+button1.addEventListener("click",verifica1)
+
+var button2 = document.querySelector("#submit1");
+button2.addEventListener("click",verifica2)
