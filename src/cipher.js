d4b24b0d56ffad49bd9794a0def12f9e26b89a7f
MarcellaLeite <marcellasaleite@gmail.com>
2023-02-13 13:11:33 -0300

    NOVO

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,25 @@
-const cipher = {
-    encode: function (deslocamento, textarea1) {        
-        if (typeof textarea1 !== "string" || textarea1 === "" || deslocamento === "" || textarea1 === 0 || deslocamento ===0) {
-        throw new TypeError;
-         }
-
-         let mensagem = ""
-        for (let i = 0; i < textarea1.length; i++) {
-            const formula = ((textarea1.charCodeAt(i) - 65 + deslocamento) % 26) + 65;
-            mensagem += String.fromCharCode(formula);
-        }
-        return mensagem
-    },
-    decode: function (deslocamento, textarea1) {
-        if (typeof textarea1 !== "string" || textarea1 === "" || deslocamento ==="" || textarea1 === 0 || deslocamento ===0) {
-            throw new TypeError;
-             }
-        let mensagem = ""
-        for (let i = 0; i < textarea1.length; i++) {
-            const formula = ((textarea1.charCodeAt(i) - 65 - deslocamento) % 26) + 65;
-            mensagem += String.fromCharCode(formula);
-        } 
-        return mensagem
+const cipher = {  
+  encode: function (textarea1, deslocamento) { 
+    if (!textarea1 || !deslocamento) {
+      throw new TypeError('bad argurments');
     }
-};
-export default cipher.js;
+    let mensagem = ""
+    for (let i = 0; i < textarea1.length; i++) {  
+      const formula = ((textarea1.charCodeAt(i) - 65 + deslocamento) % 26) + 65;
+      mensagem += String.fromCharCode(formula);}
+    return mensagem},
+
+  decode: function (textarea1, deslocamento) {
+    if (!textarea1 || !deslocamento) {
+      throw new TypeError('bad argurments');
+    }
+    let mensagem = ""       
+    for (let i = 0; i < textarea1.length; i++) {
+      const formula = ((textarea1.charCodeAt(i) - 65 - deslocamento) % 26) + 65;   
+      mensagem += String.fromCharCode(formula);} 
+    return mensagem}
+
+}
+
+
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -8,17 +8,16 @@ const button = document.querySelector("#criptografar");
 
 button.addEventListener("click", criptografar);
 
-function criptografar() { const msgCriptografada = textarea1.value.toUpperCase();
- const offset = parseInt(deslocamento.value);
- textarea2.value = cipher.encode(msgCriptografada, offset);
+function criptografar() {
+  const msgCriptografada = textarea1.value.toUpperCase();
+  const offset = parseInt(deslocamento.value);
+  textarea2.value = cipher.encode(msgCriptografada, offset);
 }
-
 const button2 = document.querySelector("#descriptografar");
-
 button2.addEventListener("click", descriptografar);
 
 function descriptografar() {
-    const msgDescriptografada = textarea1.value.toUpperCase();
-    const offset = parseInt(deslocamento.value);
-    textarea2.value = cipher.decode(msgDescriptografada, offset);
+  const msgDescriptografada = textarea1.value.toUpperCase();
+  const offset = parseInt(deslocamento.value);
+  textarea2.value = cipher.decode(msgDescriptografada, offset);
 }
